317eecd0ddb51dd6694e4be0b2a7a1c2972120a1
Kim Hwiwon <snoopy3476@outlook.com>
2021-06-27 07:52:38 +0900

    [ADD] Move MYSQL account info variables to discrete config file

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # Wordpress Backup
 A shell script which creates a full backup of a Wordpress site
 ## Prepare
-  - Set Mysql admin user ID/PW to `$MYSQL_ID` and `$MYSQL_PW` at the top of the script
-  - Change the permissions
-    - `sudo chown root:root ./wordpress-backup.sh`
-    - `sudo chmod 700 ./wordpress-backup.sh`
+  - Create a new mysql user account which has admin privilege to backup wordpress data, if does not exist
+  - If you want to enter mysql admin ID/PW automatically on each script run,
+    - Copy config template to config file: `cp mysql-info.config.template mysql-info.config`
+    - Set the mysql admin ID/PW to `$MYSQL_ID` and `$MYSQL_PW` in the config file 'mysql-info.config'
+    - Change the permissions
+      - `sudo chown 0:0 ./mysql-info.config`
+      - `sudo chmod 600 ./mysql-info.config`
 ## Usage
-`sudo ./wordpress-backup.sh <wordpress-root-dir> <backup-root-dir> [comments-for-the-backup]`
+`sudo ./wordpress-backup.sh <wordpress-root-dir> [comments-for-the-backup]`
   - **wordpress-root-dir**: Root directory of the wordpress
-  - **backup-root-dir**: Root directory of the backup
   - **comments-for-the-backup**: (Optional) Comments to append to the backup filename
 ## Job lists of the script
-  - **Dump Wordpress DB**: Make a whole dump of wordpress DB (mysql), then put it into the wordpress root dir
-  - **Backup Wordpress Data Files**: Archive and compress the wordpress root dir, then put it into the backup root dir
+  - **Dump Wordpress DB**: Make a whole dump of wordpress DB (mysql)
+  - **Backup Wordpress Data Files**: Archive and compress the DB dumpfile above the wordpress root dir
--- wordpress-backup.sh
+++ wordpress-backup.sh
@@ -1,110 +1,182 @@
 #!/bin/bash
 
-######## MYSQL ADMIN ID/PW - FILL TO MAKE THE SCRIPT WORK ########
-MYSQL_ID=''
-MYSQL_PW=''
-##################################################################
-
-
-
-FILE_PERM=$(stat -c '%u:%g-%a' "$0")
-if [ "$FILE_PERM" != "0:0-700" ] && [ "$FILE_PERM" != "0:0-500" ]
-then
-    echo "The permissions of the script file are not set properly!"
-    echo "Execute following commands before run the scripts:"
-    echo
-    echo "  $ sudo chown 0:0 \"$0\""
-    echo "  $ sudo chmod 700 \"$0\""
-    echo
-    exit 1
-fi
-
-
-if [ $# -lt 2 ]
-then
-    echo "usage: $(basename $0) <wordpress-root> <backup-root> [comments]"
-    exit 1
-fi
-
-if [ 'root' != $(whoami) ]
-then
-    echo 'Run as root user!'
-    exit 1
-fi
-
-if ! [ -z $3 ]
-then
-    COMMENTS="-[$3]"
-fi
-
-
-WP_ROOT=$(readlink -f "$1")
-BACKUP_ROOT=$(readlink -f "$2")
-BACKUP_SAVE_NAME="$(date +'%y%m%d-%H%M%S')""$COMMENTS"
-BACKUP_FILE="$BACKUP_ROOT"/"$BACKUP_SAVE_NAME".tar.gz
+# config
 DB_MYSQLDUMP=.db.mysqldump
+
+
+
+# check required binaries
+REQ_BIN_LIST="dirname pwd sudo stat chown basename readlink date grep sed systemctl mysqldump tar pigz rm"
+NOT_EXIST_BIN_LIST=""
+
+for REQ_BIN in $REQ_BIN_LIST
+do
+	command -v "$REQ_BIN" > /dev/null 2> /dev/null
+	if [ $? -ne 0 ]
+	then
+		NOT_EXIST_BIN_LIST="$NOT_EXIST_BIN_LIST '$REQ_BIN'"
+	fi
+done
+
+if [ -n "$NOT_EXIST_BIN_LIST" ]
+then
+	echo "Required binaries do not exist: $NOT_EXIST_BIN_LIST"
+	exit 1
+fi
+
+
+
+# Check config file ownership & permissions
+SCRIPTPATH="$( cd -- '$(dirname \"$0\")' >/dev/null 2>&1 ; pwd -P )"
+
+MYSQL_INFO_FILE="$SCRIPTPATH/mysql-info.config"
+if [ -f "$MYSQL_INFO_FILE" ]
+then
+
+	FILE_PERM=$(stat -c '%u:%g-%a' "$MYSQL_INFO_FILE")
+	if [ "$FILE_PERM" != "0:0-400" ] && [ "$FILE_PERM" != "0:0-600" ]
+	then
+		echo "The permissions of the mysql config file are not set properly!"
+		echo "Execute following commands before run the script:"
+		echo
+		echo "  $ sudo chown 0:0 \"$MYSQL_INFO_FILE\""
+		echo "  $ sudo chmod 600 \"$MYSQL_INFO_FILE\""
+		echo
+		exit 1
+	fi
+
+fi
+
+
+
+# Root check
+if [ "$UID" != "0" ]
+then
+	echo "Run as root user!"
+	exit 1
+fi
+
+
+
+# Help
+if [ "$#" -lt 1 ]
+then
+	echo "usage: $(basename $0) <wordpress-root> [comments]"
+	exit 1
+fi
+
+
+
+# Args process
+WP_ROOT=$(readlink -f "$1")
+if [ -n "$2" ]
+then
+	COMMENTS="-[$2]"
+fi
+BACKUP_FILE="$(date +'%y%m%d-%H%M%S')""$COMMENTS".tar.gz
+
+
+
+
 # Load wp-config.php define vars
-eval `grep ^define "$WP_ROOT"/wp-config.php | sed -e "s/define\s*(\s*'\([^']*\)'\s*,\s*\('[^']*'\|true\|false\)\s*)\s*;.*/\1=\2/g"`
+if [ ! -f "$WP_ROOT/wp-config.php" ]
+then
+	echo "No wp-config.php found in the wordpress-root ($WP_ROOT)!"
+	exit 1
+fi
+eval $(grep "^define" "$WP_ROOT/wp-config.php" | sed -e "s/define\s*(\s*'\([^']*\)'\s*,\s*\('[^']*'\|true\|false\)\s*)\s*;.*/\1=\2/g")
+source "$MYSQL_INFO_FILE" 2> /dev/null # import mysql user info from config file
+DB_USER="$MYSQL_ID"
+DB_PASSWORD="$MYSQL_PW"
 
 
 
-# DB Admin User: This should not be empty
-DB_USER=$MYSQL_ID
-DB_PASSWORD=$MYSQL_PW
+# Stop apache2
+echo Stopping apache2...
+systemctl stop apache2
+RET=$?
+if [ $RET -ne 0 ]
+then
+	exit $RET
+fi
 
+
+
+# Dump WP DB
+echo Backing up DB...
+
+# check DB id/pw from prompt
 if [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ]
 then
-    echo 'Fill your DB id and password to the variables $MYSQL_ID and $MYSQL_PW'
-    echo 'before you execute this script!'
-    exit 1
+	echo
+	echo "No DB id/password found in the config file."
+	echo
+	echo "Set your DB id/password to the variables \$MYSQL_ID and \$MYSQL_PW"
+	echo "inside the mysql config file \"$MYSQL_INFO_FILE\""
+	echo "if you want to pass them automatically."
+	echo
+	echo "  Ex)"
+	echo "	MYSQL_ID='mysql-admin-account-id'"
+	echo "	MYSQL_PW='mysql-admin-account-pw'"
+	echo
+
+
+	# get id from stdin
+	echo -n "Mysql admin ID: "
+	read DB_USER
+	# get pw from stdin, without echo
+	echo -n "Mysql password: "
+	read -s DB_PASSWORD
+	echo
+
+	# recheck id/pw
+	if [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ]
+	then
+		echo "Mysql ID and password cannot be empty!"
+		exit 1
+	fi
+
 fi
 
+mysqldump --add-drop-table --single-transaction --routines --triggers --databases "$DB_NAME" -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" > "$DB_MYSQLDUMP"
 
-
-
-echo Stopping apache2...
-service apache2 stop
 RET=$?
 if [ $RET -ne 0 ]
 then
-    exit $RET
+	exit $RET
 fi
 
-echo Backing up DB...
-mysqldump --add-drop-table --single-transaction --routines --triggers --databases "$DB_NAME" -h "$DB_HOST" -u "$DB_USER" -p$DB_PASSWORD > "$WP_ROOT/$DB_MYSQLDUMP"
 
-RET=$?
-if [ $RET -ne 0 ]
-then
-    exit $RET
-fi
 
+# Archive WP DB & Files
 echo Backing up WP...
-tar -Pc "$WP_ROOT" | pigz > "$BACKUP_FILE";
+tar -Pc "$DB_MYSQLDUMP" -C "$WP_ROOT" . | pigz > "$BACKUP_FILE"
+
 RET=$?
+rm -f "$DB_MYSQLDUMP" 2> /dev/null # remove db dump after archive
 if [ $RET -ne 0 ]
 then
-    exit $RET
+	exit $RET
 fi
 
-rm -f "$WP_ROOT/$DB_MYSQLDUMP" 2> /dev/null
 
 
+
+# Restart apache2
 echo Starting apache2...
-service apache2 start
+systemctl start apache2
 RET=$?
 if [ $RET -ne 0 ]
 then
-    exit $RET
+	exit $RET
 fi
 
 
 
-id $USER > /dev/null 2> /dev/null
-RET=$?
-if [ $RET -ne 0 ]
+# Change owner to the user before sudo
+if [ -z "$SUDO_UID" ] || [ -z "$SUDO_GID" ]
 then
-    exit $RET
+	exit 1
 fi
-#chown -R $USER.$USER "$BACKUP_SAVE_PATH"
+chown "$SUDO_UID":"$SUDO_GID" "$BACKUP_FILE"
 
